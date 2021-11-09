ca5be4dccf7b1f3945c1047c22a73de5ee6beee1
WIZZ-871 <93987855+WIZZ-871@users.noreply.github.com>
2021-11-09 10:49:24 -0500

    imprimir en python

---
+++ main.py
@@ -0,0 +1,15 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+var_string = "Hello world"
+var_numerica = 123
+var_float = 123.3435
+
+print(var_string)
+print(type(var_string))
+
+print(var_numerica)
+print(type(var_numerica))
+
+print(var_float)
+print(type(var_float))
