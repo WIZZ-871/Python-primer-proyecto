d9c893cb8e1816a48d068d8f1a799da3f7c0370e
daniel alejandro chaparro zipa <X442UR@ASUS.X442UR>
2021-11-22 09:08:34 -0500

    calculo area circulo

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
-
-"sumar 2 numeros"
+import math import pi
+#sumar 2 numeros"
 
 num_1 = 31
 num_2 = 43
 suma1 = num_1 + num_2
 print (suma1)
 
-"sumar 2 float"
+#sumar 2 float"
 float_1 = 15.22
 float_2 = 10.28
 suma2 = float_1 + float_2
 print (suma2)
 
-"concatenar 2 string"
+#concatenar 2 string"
 
 nombre = "Daniel Alejandro"
 apellido = "Chaparro Zipa"
