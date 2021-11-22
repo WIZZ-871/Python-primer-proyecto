999e68efda58979e0e1fa5486d82b44c6bc40003
daniel alejandro chaparro zipa <X442UR@ASUS.X442UR>
2021-11-22 12:10:55 -0500

    saber si un numero es par o impar

---
+++ homework_math/saber_si_es_par_o_impar.py
@@ -0,0 +1,10 @@
+# mirar si un numero es par o impar
+
+numero = int(input("digite un numero para saber si es par o impar: \n "))
+
+if numero%2==0:
+    print("el numero ingresado es par")
+else:
+    print("el numero ingresado es impar")
+
+
